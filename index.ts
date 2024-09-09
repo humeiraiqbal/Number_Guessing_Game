85ca984f56bff86d7b7c4719fcd83840e7436cfb
humeiraiqbal <humeiraiqbal1@gmail.com>
2024-09-10 04:30:49 +0500

    Add files via upload
    Pushing Number Guessing Game to Github

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random());
+console.log(randomNumber *6 +1);
+ 
+
+let answer = await inquirer.prompt([{
+
+name: "userGuessedNumber",
+type: "number",
+message: "Please guess a number."
+
+}]);
+
+if (answer.userGuessedNumber === randomNumber){
+
+    console.log("Congratulations! you guess the correct number.");
+    
+}else{
+    console.log("OOps! Try again");
+    
+}
